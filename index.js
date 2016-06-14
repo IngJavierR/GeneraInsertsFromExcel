3c5a94e2154fc79efc7ec5cbb9d4190ffbb35212
JavierRodriguez <jrodriguez@apesa.com.mx>
2016-06-14 17:56:36 -0500

    Manipula array

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 (function(){
   'use strict';
 
-  
+  console.log('Leyendo excel');
+
+  var arr = ['item 1', 'item 2', 'item 3'];
+  var list = '<ul><li>' + arr.join('</li><li>') + '</li></ul>';
+  console.log(list);
+
 
 })();
