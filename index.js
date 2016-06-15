d3db85fb4ffcea908b0ea538d796ac6ebf71431c
IngJavierR <hazelapd@gmail.com>
2016-06-14 22:51:06 -0500

    Se formatea index

--- index.js
+++ index.js
@@ -8,37 +8,39 @@
 
     var filename = "./exceldocs/productosImas.csv";
     var inserts = [];
-    //insert into Productos values (2, 'RE002', 'AHUJA CARGADA'				, 'AHUJA CARGADA'				, 'AHUJA CARGADA DESC'				, '', 1);
+
     var workbook = new Excel.Workbook();
     workbook.csv.readFile(filename)
         .then(function(worksheet) {
             worksheet.eachRow({
                 includeEmpty: true
             }, function(row, rowNumber) {
-              var imagen64 = convertToBase64(row.getCell(4).value);
-              var data = "insert into Productos values (" + rowNumber + ", '" + row.getCell(1).value.trim() + "', '" + row.getCell(1).value.trim() + "', '" + row.getCell(1).value.trim() + "', '"+ row.getCell(1).value.trim() + " DESC" + "', '" + imagen64 + "', 1);"
-              inserts.push(data);
-              console.log("Row " + data);
+                var imagen64 = convertToBase64(row.getCell(4).value);
+                var data = "insert into Productos values (" + rowNumber + ", '" +
+                    row.getCell(1).value.trim() + "', '" + row.getCell(1).value.trim() + "', '" + row.getCell(1).value.trim() + "', '" +
+                    row.getCell(1).value.trim() + " DESC" + "', '" + imagen64 + "', 1);"
+                inserts.push(data);
+                console.log("Row " + data);
             });
             writeFile(inserts);
             console.log('Generacion terminada');
         });
 
 
-    function convertToBase64(pathImagen){
-      if(!pathImagen) return '';
-      var bitmap = fs.readFileSync('./imagenes/' + pathImagen.trim());
-      return new Buffer(bitmap).toString('base64');
+    function convertToBase64(pathImagen) {
+        if (!pathImagen) return '';
+        var bitmap = fs.readFileSync('./imagenes/' + pathImagen.trim());
+        return new Buffer(bitmap).toString('base64');
     };
 
-    function writeFile(content){
+    function writeFile(content) {
         var stream = fs.createWriteStream("inserts.sql");
         stream.once('open', function(fd) {
-          content.map(function(dat){
-            stream.write(dat + '\n');
-          });
-        stream.end();
-      });
+            content.map(function(dat) {
+                stream.write(dat + '\n');
+            });
+            stream.end();
+        });
     }
 
 })();
